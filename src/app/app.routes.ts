207d9940202f89baaad3bb135632facbad63a9fc
John Doe <johndoe@example.com>
2025-12-15 18:09:50 +0700

    662110071_chom

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { HelloWorld } from './hello-world/hello-world';
+import { Profile } from './profile/profile';
+import { Friend } from './friend/friend';
+import { Status } from './status/status';
 
 export const routes: Routes = [
   {
@@ -9,4 +12,25 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   { path: 'hello-world', component: HelloWorld },
+  {
+    path: '',
+
+    redirectTo: 'profile',
+    pathMatch: 'full',
+  },
+  { path: 'profile', component: Profile },
+  {
+    path: '',
+
+    redirectTo: 'friend',
+    pathMatch: 'full',
+  },
+  { path: 'friend', component: Friend },
+  {
+    path: '',
+
+    redirectTo: 'status',
+    pathMatch: 'full',
+  },
+  { path: 'status', component: Status },
 ];
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.html',
+  styleUrl: './profile.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Profile {}
